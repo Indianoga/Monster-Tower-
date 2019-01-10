6d4482795106f59195a5396cd75e206a3a2e0e1c
Gabriel Oliveira <augusto.gabrielmorais@gmail.com>
2019-01-10 11:43:23 -0200

    Store System ++

---
+++ Assets/_ 01 Scripts/StoreManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StoreManager : MonoBehaviour 
+{
+	[SerializeField]
+	Text gold;
+	int currentGold;
+	[SerializeField]
+	public ItensControls[] itensControls;
+
+	// Use this for initialization
+	void Start () 
+	{
+		currentGold = PlayerPrefs.GetInt("gold");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		gold.text = currentGold.ToString();
+	}
+
+	public void Buy(string item)
+	{
+		if (item == "Shield")
+		{
+			if(currentGold > itensControls[0].price)
+			{
+				Debug.Log("I can Buy");
+			}
+			else
+			{
+				Debug.Log("I need more money");
+			}
+		}
+		
+	}
+}
